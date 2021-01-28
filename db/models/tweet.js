1c2117d1387a8c13ad2af1614ee4028df19bec7d
Alex Rodriguez <alex@email.com>
2021-01-28 17:03:29 -0400

    Phase 2 done

--- db/models/tweet.js
+++ db/models/tweet.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
     message: {
-     type =  DataTypes.STRING(280),
+     type :  DataTypes.STRING(280),
      allowNull: false
     }
   }, {});
